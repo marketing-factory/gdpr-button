df88a9fb6c1470b57eb08754d606871bf22eece8
Christian Spoo <cs@marketing-factory.de>
2018-06-06 12:36:25 +0200

    First draft source code

---
+++ gdpr-button.ino
@@ -0,0 +1,28 @@
+#include <Keyboard.h>
+
+const int buttonPin = 2;
+const char lockSequence[] = {
+  KEY_LEFT_CTRL,
+  KEY_LEFT_GUI, // Left-Cmd-Key / Left-Windows-Key
+  'q'
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  Keyboard.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (LOW == digitalRead(buttonPin)) {
+    for (int i = 0; i < sizeof(lockSequence); i++) {
+      Keyboard.press(lockSequence[i]);
+    }
+    delay(100);
+    Keyboard.releaseAll();
+  }
+
+  delay(500);
+}
